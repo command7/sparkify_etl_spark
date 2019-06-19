00f639cad3c78b43b7637791d1c9e9a568b4482e
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2019-06-19 17:31:53 -0400

    Update: Change config file variable names

--- etl.py
+++ etl.py
@@ -3,4 +3,4 @@ import configparser
 conf_parser = configparser.ConfigParser()
 conf_parser.read_file(open('credentials.cfg', 'r'))
 
-print(conf_parser['AWS']['access_key'])
+print(conf_parser['AWS']['AWS_SECRET_KEY'])
