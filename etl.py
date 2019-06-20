9eb08ad0458408c077ac7217d2457490c43fe852
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2019-06-20 18:52:31 -0400

    Feat: Set application name for spark session

--- etl.py
+++ etl.py
@@ -2,27 +2,28 @@ import configparser
 from pyspark.sql import SparkSession
 import os
 
-conf_parser = configparser.ConfigParser()
-with open('etl_config.cfg', 'r') as config_file:
-    conf_parser.read_file(config_file)
-
-aws_access = conf_parser['AWS']['AWS_ACCESS_KEY']
-aws_secret = conf_parser['AWS']['AWS_SECRET_KEY']
-
-spark = SparkSession.builder.config("spark.jars.packages",
-                                    "org.apache.hadoop:hadoop-aws:2.7.0")\
-    .getOrCreate()
-
-log_data = spark.read.json(path='log-data/*')
-song_data = spark.read.json(path='song_data/*/*/*/*.json')
-log_data.show()
-song_data.show()
+# conf_parser = configparser.ConfigParser()
+# with open('etl_config.cfg', 'r') as config_file:
+#     conf_parser.read_file(config_file)
+#
+# aws_access = conf_parser['AWS']['AWS_ACCESS_KEY']
+# aws_secret = conf_parser['AWS']['AWS_SECRET_KEY']
+#
+# spark = SparkSession.builder.config("spark.jars.packages",
+#                                     "org.apache.hadoop:hadoop-aws:2.7.0")\
+#     .getOrCreate()
+#
+# log_data = spark.read.json(path='log-data/*')
+# song_data = spark.read.json(path='song_data/*/*/*/*.json')
+# log_data.show()
+# song_data.show()
 
 
 def initiate_session():
     spark = SparkSession.builder\
         .config("spark.jars.packages",
                 "org.apache.hadoop:hadoop-aws:2.7.0")\
+        .appName("sparkify_etl")\
         .getOrCreate()
     return spark
 
